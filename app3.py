9169cc8988fd69d85d296343576b866ea41dc1bd
Dev-Pakorn <papangkorn.ni.68@ubu.ac.th>
2026-01-30 16:51:39 +0700

    add app3

--- app3.py
+++ app3.py
@@ -5,7 +5,7 @@ oop = ClassRoom("OOP")
 oop.add_student(Student(1, "Alice", 20, "S1001"))
 oop.add_student(Student(2, "Bob", 22, "S1002"))
 print(f"{oop.name} registered {len(oop)} students")
-oop.add_student(Student(3, "Charlie", 21, "S003"))
+oop.add_student(Student(3, "Charlie", 21, "S1003"))
 print(len(oop))
 print("Students in the class:")
 for i in range(len(oop)):
